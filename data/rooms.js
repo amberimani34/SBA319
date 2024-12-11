cfc52217d2aae8de22aa39c1323aaf75c39d1b88
Amber Roberts <a.roberts_90@yahoo.com>
2024-12-10 20:56:35 -0600

    updated last data file

--- data/rooms.js
+++ data/rooms.js
@@ -1,52 +1,31 @@
-[
+const { Router } = require("express");
+const mongoose = require("mongoose");
+
+const roomsSchema = new mongoose.Schema(
     {
-      "_id": 1,
       "project_id": 1,
       "room_name": "Living Room",
-      "dimensions": {
-        "length": 20,
-        "width": 15,
-        "area": 300
-      },
       "function": "Socializing",
       "design_style": "Modern",
-      "furniture": [
-        { "furniture_id": 1, "quantity": 1 },
-        { "furniture_id": 2, "quantity": 1 },
-        { "furniture_id": 3, "quantity": 2 }
-      ]
+      "furniture count": 5
     },
     {
-      "_id": 2,
       "project_id": 1,
       "room_name": "Kitchen",
-      "dimensions": {
-        "length": 15,
-        "width": 12,
-        "area": 180
-      },
       "function": "Cooking",
       "design_style": "Contemporary",
-      "furniture": [
-        { "furniture_id": 4, "quantity": 1 },
-        { "furniture_id": 5, "quantity": 3 }
-      ]
+      "furniture count": 8
     },
     {
-      "_id": 3,
       "project_id": 2,
       "room_name": "Office",
-      "dimensions": {
-        "length": 18,
-        "width": 14,
-        "area": 252
-      },
       "function": "Work",
       "design_style": "Minimalist",
-      "furniture": [
-        { "furniture_id": 6, "quantity": 1 },
-        { "furniture_id": 7, "quantity": 1 }
-      ]
+      "furniture": 4
     }
-  ]
+  )
+
+  roomsSchema.index({ size: 1 });
+
+module.exports = mongoose.model("Rooms", roomsSchema);
   
